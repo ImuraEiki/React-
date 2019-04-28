132ba53d680da381a666a34556abd56c34421809
ImuraEiki <iimura1560222@gmail.com>
2019-04-28 18:56:50 +0900

    配列を読み込むやつかいた

--- App.js
+++ App.js
@@ -2,15 +2,33 @@ import React, {Component} from 'react'
 import Table from './Table'
 
 
-class App extends React.Component{
-    render(){
-        return(
-            <div className="App">
-                <h1>Hello, React!</h1>
-                <Table />
-            </div>
-        )
+class App extends Component {
+    render() {
+      const characters = [
+        {
+          name: 'Charlie',
+          job: 'Janitor',
+        },
+        {
+          name: 'Mac',
+          job: 'Bouncer',
+        },
+        {
+          name: 'Dee',
+          job: 'Aspring actress',
+        },
+        {
+          name: 'Dennis',
+          job: 'Bartender',
+        },
+      ]
+  
+      return (
+        <div className="container">
+          <Table characterData={characters}/>
+        </div>
+      )
     }
-}
+  }
 
 export default App
