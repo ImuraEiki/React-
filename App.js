342db61334e90cdb4f38c0f1f451137f138ca46e
ImuraEiki <iimura1560222@gmail.com>
2019-04-29 12:27:15 +0900

    my first commit

--- App.js
+++ App.js
@@ -1,44 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react'
 
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
-
-//親component
-class App extends Component {
-  state = {
-    username: 'supermax'
-  }
-
-  usernameChangeHandler = (event) => {
-    this.setState({username: event.target.value});
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <ol>
-          <li>Create TWO new components: UserInput and UserOutput</li>
-          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
-          <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
-          <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
-          <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
-          <li>Add a method to manipulate the state (=> an event-handler method)</li>
-          <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
-          <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
-          <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
-          <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
-        </ol>
-        {/* propatiesは属性をつけるのと同じやり方でつける。名前は自分で決めるっぽい*/}
-        <UserInput 
-          changed={this.usernameChangeHandler}
-          currentName={this.state.username} /> 
-        <UserOutput userName={this.state.username} />
-        <UserOutput userName={this.state.username} />
-        <UserOutput userName="MAX" />
-      </div>
-    );
-  }
+function SearchBar(){
+    return(
+        <form>
+            <label>
+                <input type="text" name="name" />
+            </label>
+        </form>
+    )
 }
 
-export default App;
+class ProductTable extends React.Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+
+class App extends React.Component{
+    render(){
+        return(
+            <div className="App">
+                <SearchBar />
+                <ProductTable />
+            </div>
+        )
+    }
+}
+
+export default App
